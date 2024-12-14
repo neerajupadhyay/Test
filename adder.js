122612f65d3995a15d2aed72e7b37eb3030435ba
neerajupadhyay <er.neerajupadhyay@gmail.com>
2024-12-14 12:16:18 +0530

    String Calculator TDD

--- adder.js
+++ adder.js
@@ -1,8 +1,37 @@
 
-function add() {
-    console.log('testtt')
-    return 0;
+function add(sampleString) {
+  if(sampleString && sampleString.length > 1){
+    // check negative numbers
+    const negatives = findNegativeNumbers(sampleString);
+    if(negatives.length>0){
+    return `Negative numbers not allowed ${negatives.join(", ")}.`
+    }
+    let sum = 0;
+    let reference = "0123456789";
+    let numbers = [...sampleString].reduce((acc, char) => reference.includes(char) ? acc + char : acc, '');
+
+    for(let num of numbers){
+        sum += parseInt(num);
+    }
+    return sum
+  }else{
+    if(sampleString == 1){
+        return 1
+    }else{
+        return 0
+    }
+   
   }
-  
+    
+  }
+  function findNegativeNumbers(str) {
+    // Regular expression to match negative numbers
+    const regex = /-?\d+(\.\d+)?/g;
+    // Find all matches in the string
+    const matches = str.match(regex);
+    // Filter out positive numbers
+    const negativeNumbers = matches ? matches.filter(num => parseFloat(num) < 0) : [];
+    return negativeNumbers;
+}
 
 export default add
--- test/adder.spec.js
+++ test/adder.spec.js
@@ -2,8 +2,28 @@ import { assert } from "chai";
 import add from '../adder.js';
 
 describe("adder",()=>{
-    it("should retiurn 0" ,()=>{
-        const result = add();
+    it("it should return 0 for blank string" ,()=>{
+        const result = add("");
         assert.equal(result,0)
+    });
+    it("it should return 1" ,()=>{
+        const result = add("1");
+        assert.equal(result,1)
+    });
+    it("it should return addition of string '1,5' " ,()=>{
+        const result = add("1,5");
+        assert.equal(result,6)
+    });
+    it("Allow the add method to handle new lines between numbers (instead of commas). ('1\n2,3' should return 6)" ,()=>{
+        const result = add("1\n2,3");
+        assert.equal(result,6)
+    });
+    it("To change the delimiter, the beginning of the string will contain a separate line that looks like this '//;\n1;2' where the delimiter is ';' should return 3." ,()=>{
+        const result = add("//;\n1;2");
+        assert.equal(result,3)
+    });
+    it("Calling add with a negative number will throw an exception: 'negative numbers not allowed '1543-8'" ,()=>{
+        const result = add("1543-8");
+        assert.equal(result,'Negative numbers not allowed -8.')
     });
 });
